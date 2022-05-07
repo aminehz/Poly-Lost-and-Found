048aa592320127fcac63cfcf7f2a951eeccac535
amine.hz <amine.hz.hz.98@gmail.com>
2022-05-07 19:00:52 +0100

    version 0.2

--- src/FoundItems.js
+++ src/FoundItems.js
@@ -10,14 +10,7 @@ console.log(filter.data);
 
   return (  
     <div className="home">
-          { blogs && (
-        <article>
-          <h2>{ blogs.title }</h2>
-          <p>Written by { blogs.author }</p>
-          <div>{ blogs.body }</div>
-          <p>the object is {blogs.isFound}</p>
-        </article>
-      )}
+    
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> } 
     {blogs &&<BlogList blogs={blogs} /> }
